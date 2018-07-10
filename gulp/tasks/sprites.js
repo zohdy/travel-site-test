4555f3e557ebc85be2f9128272eda07e793f8972
Patrick Peter Zohdy <zohdy@me.com>
2018-07-11 00:09:09 +0200

    Completed automated sprite generation

---
+++ gulp/tasks/sprites.js
@@ -0,0 +1,45 @@
+let gulp = require('gulp');
+let svgSprite = require('gulp-svg-sprite');
+let rename = require('gulp-rename');
+let clear = require('del');
+
+let config = {
+     mode: {
+         css: {
+             sprite: 'sprite.svg',
+             render: {
+                css: {
+                    template: './gulp/templates/sprite.css'
+                }
+            }  
+         }
+     }
+}
+
+gulp.task('beginClean', function(){
+    return clear(['./app/temp/sprite', './app/assets/images/sprites']);
+});
+
+gulp.task('createSprite', ['beginClean'], function(){
+    return gulp.src('./app/assets/images/icons/**/*.svg')
+        .pipe(svgSprite(config))
+        .pipe(gulp.dest('./app/temp/sprite'));
+});
+
+gulp.task('copySpriteGraphic', ['createSprite'], function(){
+    return gulp.src('./app/temp/sprite/css/**/*.svg')
+        .pipe(gulp.dest('./app/assets/images/sprites'));
+});
+
+gulp.task('copySpriteCSS', ['createSprite'], function(){
+    gulp.src('./app/temp/sprite/css/*.css')
+        .pipe(rename('_sprite.css'))
+        .pipe(gulp.dest('./app/assets/styles/modules'));
+});
+
+gulp.task('endClean', ['copySpriteGraphic', 'copySpriteCSS'], function(){
+    return clear('./app/temp/sprite');
+})
+
+gulp.task('icons', ['beginClean','createSprite', 'copySpriteGraphic', 'copySpriteCSS', 'endClean']);
+
